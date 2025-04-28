b39047b57ed6de2275a0fa676ed166521798ea88
PM <payam.ranjbar@live.com>
2025-04-28 15:21:49 -0600

    Separated animation logic from actor

--- Assets/Scripts/Agents/Utils/ActorAgentAnimator.cs
+++ Assets/Scripts/Agents/Utils/ActorAgentAnimator.cs
@@ -1,11 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace Agents.Utils
 {
     public class ActorAgentAnimator : MonoBehaviour
     {
         [SerializeField] private Animator animator;
+        [SerializeField] private float bodyRotationCheckRadius;
+        [SerializeField] private Rig headRotationRig;
+        [SerializeField] private Transform headTarget;
+        private float _lookDuration = 1;
+        private Vector3 _initHeadPos;
+
+
+        private void Start()
+        {
+            _initHeadPos = headTarget.position;
+        }
+        public IEnumerator LookAt(AgentTarget target, bool walk = false)
+        {
+            // send the target to the location 
         
+            // animate weight of rig to 1
+            RealizeLookDuration();
+            if(walk || CheckForBodyRotation(target.Coordinate))
+                yield return RotateBody(target);
+            yield return headTarget.DOMove(target.Coordinate, _lookDuration);
+
+
+        }
+
+        public IEnumerator RotateBody(AgentTarget target)
+        {
+            yield return transform.DOLookAt(target.Coordinate, _lookDuration);
+        }
+
+        private IEnumerator ResetLook()
+        {
+            yield return headTarget.DOMove(_initHeadPos, _lookDuration);
+        }
+
         public void SetTrigger(string triggerName, int layerIndex = 0)
         {
             if (animator == null || string.IsNullOrEmpty(triggerName))
@@ -19,6 +55,16 @@ namespace Agents.Utils
 
             animator.ResetTrigger(triggerName);                     
             animator.SetTrigger(triggerName);
+        }
+
+        private bool CheckForBodyRotation(Vector3 targetPosition)
+        {
+            var position = transform.position;
+            var dirVec = new Vector3(targetPosition.x, position.y, targetPosition.z) - position;
+
+            var dot = Vector3.Dot(transform.forward, dirVec);
+
+            return dot <= 0;
         }
 
         private bool IsAnimPlaying(string triggerName, int layerIndex)
@@ -43,6 +89,11 @@ namespace Agents.Utils
             }
 
             return false;
+        }
+        
+        private float RealizeLookDuration()
+        {
+            return 1f;
         }
     }
 }
