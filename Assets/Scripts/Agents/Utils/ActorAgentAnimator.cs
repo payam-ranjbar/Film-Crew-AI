afd45e6ee6f3a48ac8e01121d355f61a53faa401
PM <payam.ranjbar@live.com>
2025-04-28 15:13:16 -0600

    Actor Animator Added

---
+++ Assets/Scripts/Agents/Utils/ActorAgentAnimator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Agents.Utils
+{
+    public class ActorAgentAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        
+        public void SetTrigger(string triggerName, int layerIndex = 0)
+        {
+            if (animator == null || string.IsNullOrEmpty(triggerName))
+                return;
+
+            var hasTrigger = HasTrigger(triggerName);
+
+            if (!hasTrigger) triggerName = "idle";
+            
+            if (IsAnimPlaying(triggerName, layerIndex)) return;
+
+            animator.ResetTrigger(triggerName);                     
+            animator.SetTrigger(triggerName);
+        }
+
+        private bool IsAnimPlaying(string triggerName, int layerIndex)
+        {
+            var stateTag = triggerName;
+            int targetHash = Animator.StringToHash(stateTag);
+            if (animator.GetCurrentAnimatorStateInfo(layerIndex).tagHash == targetHash)
+                return true;
+            return false;
+        }
+
+        private bool HasTrigger(string triggerName)
+        {
+            for (var i = 0; i < animator.parameters.Length; i++)
+            {
+                var p = animator.parameters[i];
+                if (p.type == AnimatorControllerParameterType.Trigger &&
+                    p.name == triggerName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
